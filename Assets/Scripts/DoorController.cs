ac4061ad9d27c2e9c7f54c14c5e6f8edf0eeb400
Alexander Shearer <sheareraws@gmail.com>
2019-09-08 15:21:29 -0400

    base game

---
+++ Assets/Scripts/DoorController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorController : MonoBehaviour {
+    
+    private bool open = false;
+
+    void OnTriggerEnter2D(Collider2D other) {
+        if (!open && other.tag == "Shot") {
+            open = true;
+            GetComponent<Animator>().SetTrigger("Open");
+        }
+    }
+
+    void DisableCollider() {
+        GetComponent<Collider2D>().enabled = false;
+    }
+}
