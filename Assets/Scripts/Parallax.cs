57f457a5d4932e665b8d6419179bf0c80e000a1b
Alexander Shearer <sheareraws@gmail.com>
2019-09-08 20:43:49 -0400

    adds parallax background

---
+++ Assets/Scripts/Parallax.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour {
+    
+    private Vector3 initCameraPos;
+    private Vector3 initPos;
+
+    public Transform mainCamera;
+
+    public float xrate, yrate;
+
+    void Start() {
+        initCameraPos = mainCamera.position;
+        initPos = transform.position;
+    }
+
+    void LateUpdate() {
+        Vector3 newPos = initPos;
+        newPos.x += (mainCamera.position - initCameraPos).x * xrate;
+        newPos.y += (mainCamera.position - initCameraPos).y * yrate;
+        transform.position = newPos;
+    }
+
+
+}
